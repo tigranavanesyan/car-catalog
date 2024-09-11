5afe15de7ac9ecf358487de6a78c2dac7179e8e9
tigranavanesyan <tigran19910315@gmail.com>
2024-09-11 18:32:29 +0400

    scroll on hover

--- src/components/PartHighlighter2.js
+++ src/components/PartHighlighter2.js
@@ -1,35 +1,51 @@
-import React, { useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 
 // Component to render image map and table
 const PartHighlighter = () => {
     const [highlightedPart, setHighlightedPart] = useState(null);
+    const [hoverOnList, setHoverOnList] = useState(false);
 
-    const parts = [
-        { id: '09110', name: 'Wrench, Hub Nut Box', range: '12.2001 - 12.2004', price: '11.00', coords: { top: '70px', left: '85px', width: '80px', height: '20px' } },
-        { id: '09113', name: 'Spanner', range: '12.2004 - ...', price: '2.29', coords: { top: '100px', left: '475px', width: '80px', height: '20px' } },
-        { id: '09120', name: 'Pliers', range: '12.2001 - 12.2004', price: '3.82', coords: { top: '247px', left: '149px', width: '80px', height: '20px' } },
-        // Add more parts and coordinates as necessary
-    ];
+    const listElement = useRef(null);
+
+    useEffect(() => {
+        if(!hoverOnList){
+            const currentHighlight = listElement.current.querySelector('.grid_section.highlight');
+            currentHighlight?.scrollIntoView({block:"nearest", behavior: 'smooth',inline:"start"});
+        }
+    }, [highlightedPart,hoverOnList]);
+
     const parts2 = [
         {
             id: '09110',
             name: 'JACK ASSY',
-            coords: { top: '100px', left: '475px', width: '80px', height: '20px' },
+            coords: { top: '6%', left: '11%', width: '11%', height: '2.5%' },
             img:"",
             items: [
                 {id: '09111-22060',name: 'MCV20',range: '08.1996 - 05.1998', req:"01", price: '56.39'},
                 {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
                 {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'}
             ]
         },
         {
             id: '09113',
             name: 'JACK ASSY',
-            coords: { top: '70px', left: '85px', width: '80px', height: '20px' },
+            coords: { top: '8.8%', left: '62%', width: '11%', height: '2.5%' },
             img:"",
             items: [
                 {id: '09111-22060',name: 'MCV20',range: '08.1996 - 05.1998', req:"01", price: '56.39'},
+                {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
+                {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
                 {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
                 {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'}
             ]
@@ -37,103 +53,82 @@ const PartHighlighter = () => {
         {
             id: '09120',
             name: 'JACK ASSY',
-            coords: { top: '247px', left: '149px', width: '80px', height: '20px' },
+            coords: { top: '21.8%', left: '19%', width: '11%', height: '2.5%' },
             img:"",
             items: [
+                {id: '09111-22060',name: 'MCV20',range: '08.1996 - 05.1998', req:"01", price: '56.39'},
+                {id: '09111-22060',name: 'MCV20',range: '08.1996 - 05.1998', req:"01", price: '56.39'},
+                {id: '09111-22060',name: 'MCV20',range: '08.1996 - 05.1998', req:"01", price: '56.39'},
                 {id: '09111-22060',name: 'MCV20',range: '08.1996 - 05.1998', req:"01", price: '56.39'},
                 {id: '09111-0W041',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'},
                 {id: '09111-02090',name: 'MCV20',range: '05.1998 - ...', req:"01",price: '50.66'}
             ]
         }
-
-        // Add more parts and coordinates as necessary
     ];
 
-    const handleHover = (partId) => {
+    const handleHover = (partId,bool) => {
         setHighlightedPart(partId);
     };
 
     const handleLeave = () => {
         setHighlightedPart(null);
+        setHoverOnList(false)
     };
 
     return (
         <div className="container">
-            <div className="image-container">
-                <img src="/090494.png" alt="Parts" className="image" />
+            <div className="image_wrapper">
+                <div className="image-container">
+                    <img src="/090494.png" alt="Parts" className="image"/>
 
-                {parts.map((part) => (
-                    <div
-                        key={part.id}
-                        className={`overlay ${highlightedPart === part.id ? 'hovered' : ''}`}
-                        style={{
-                            top: part.coords.top,
-                            left: part.coords.left,
-                            width: part.coords.width,
-                            height: part.coords.height,
-                        }}
-                        onMouseEnter={() => handleHover(part.id)}
-                        onMouseLeave={handleLeave}
-                    />
-                ))}
+                    {parts2.map((part) => (
+                        <div
+                            key={part.id}
+                            className={`overlay ${highlightedPart === part.id ? 'hovered' : ''}`}
+                            style={{
+                                top: part.coords.top,
+                                left: part.coords.left,
+                                width: part.coords.width,
+                                height: part.coords.height,
+                            }}
+                            onMouseEnter={() => handleHover(part.id)}
+                            onMouseLeave={handleLeave}
+                        />
+                    ))}
+                </div>
             </div>
 
-            <table className="parts-table">
-                <thead>
-                <tr>
-                    <th>OEM</th>
-                    <th>Name / Details</th>
-                    <th>Period</th>
-                    <th>Req.</th>
-                    <th>Price from, USD</th>
-                </tr>
-                </thead>
-                <tbody>
+            <div ref={listElement} className="grid_table">
+                <div className="grid_head">OEM</div>
+                <div className="grid_head">Name / Details</div>
+                <div className="grid_head">Period</div>
+                <div className="grid_head">Req.</div>
+                <div className="grid_head">Price from, USD</div>
+                <div className="grid_head">*</div>
                 {parts2.map((part) => (
-                    <tr
+                    <div
                         key={part.id}
-                        className={highlightedPart === part.id ? 'highlight' : ''}
-                        onMouseEnter={() => handleHover(part.id)}
+                        className={`grid_table1 grid_section ${highlightedPart === part.id ? 'highlight' : ''}`}
+                        onMouseEnter={() => {
+                            handleHover(part.id)
+                            setHoverOnList(true)
+                        }}
                         onMouseLeave={handleLeave}
                     >
-                        <td colSpan={6}>
-                            <table>
-                                <tbody>
-                                <tr>
-                                    <td colSpan={6}>{part.id}-{part.name}</td>
-                                </tr>
-                                {part.items.map((item) => (
-                                    <tr>
-                                        <td>
-                                            <a href={"#"}>{item.id}</a>
-                                        </td>
-                                        <td>
-                                            {item.name}
-                                        </td>
-                                        <td>
-                                            {item.range}
-                                        </td>
-                                        <td>
-                                            {item.req}
-                                        </td>
-                                        <td>
-                                            {item.price}
-                                        </td>
-                                        <td>
-                                            add to cart
-                                        </td>
-                                    </tr>
-                                ))}
-
-
-                                </tbody>
-                            </table>
-                        </td>
-
-                    </tr>
+                        <div key={part.id} className={`grid_title`}>{part.id}-{part.name}</div>
+                        {part.items.map((item) => (
+                            <div className={"grid_table1 grid_section list_row"}>
+                                <div><a href={"https://japanauction1987.netlify.app/"} target={"_blank"} rel="noreferrer">{item.id}</a></div>
+                                <div>{item.name}</div>
+                                <div>{item.range}</div>
+                                <div>{item.req}</div>
+                                <div>{item.price}</div>
+                                <div>cart</div>
+                            </div>
+                        ))}
+                    </div>
                 ))}
-                </tbody>
-            </table>
+            </div>
         </div>
     );
 };
